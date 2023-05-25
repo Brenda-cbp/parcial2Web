a327be58d76937912fee962f4749d5dfc29ef983
Brenda-cbp <bc.barahona@uniandes.edu.co>
2023-05-25 07:02:08 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
